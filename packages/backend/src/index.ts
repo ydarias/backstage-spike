7e5dde3333ca5d653ec2b1f72658fd9ab87e85de
Yeray Darias <ydarias@gmail.com>
2024-12-02 11:28:38 +0100

    Added Technology Radar Backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -51,4 +51,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
+// Tech Radar
+backend.add(import('@backstage-community/plugin-tech-radar-backend'));
+
 backend.start();
