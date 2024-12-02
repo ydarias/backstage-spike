cfc1921ca04924143c320c1f13ba2a47710c44b1
Yeray Darias <ydarias@gmail.com>
2024-12-02 10:43:02 +0100

    Added Technology Radar

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -36,6 +36,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { TechRadarPage } from '@backstage-community/plugin-tech-radar';
 
 const app = createApp({
   apis,
@@ -95,6 +96,10 @@ const routes = (
     </Route>
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
+    <Route
+      path="/tech-radar"
+      element={<TechRadarPage width={1500} height={800} />}
+    />
   </FlatRoutes>
 );
 
--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -4,6 +4,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import ExtensionIcon from '@material-ui/icons/Extension';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
+import HubIcon from '@material-ui/icons/WifiTethering';
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
 import {
@@ -75,6 +76,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={HubIcon} to="tech-radar" text="Tech Radar" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
